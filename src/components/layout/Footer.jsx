423545970e87215a4ad9bb135585af6aaabfa037
Stodulski <maxisto043@gmail.com>
2025-06-08 22:08:30 -0300

    Boton de instagram agregado

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,16 @@
 import { Logo } from '../UI/Logo'
+import igLogo from '../../assets/instagram.svg'
 const Footer = () => {
   return (
-    <footer className='p-10 bg-gradient-to-t  from-red-50 via-transparent to-transparent'>
-      <a href='https://www.instagram.com/skymait/' target='_BLANK' title='Ir al instagram' className='flex items-center justify-center gap-1'>
-        <Logo footer={true}/>
+    <footer className='p-10 bg-gradient-to-t flex justify-center items-center flex-col gap-y-10 from-red-50 via-transparent to-transparent'>
+      <a
+        href='https://www.instagram.com/skymait/'
+        target='_BLANK'
+        title='Ir al instagram'
+      >
+        <img src={igLogo} alt="Logo de instagram" className='w-16'/>
       </a>
+      <Logo footer={true} />
     </footer>
   )
 }
