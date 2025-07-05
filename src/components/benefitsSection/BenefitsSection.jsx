6cb4994ca47ba2785e343b8cbc0fcaefbce55698
Stodulski <maxisto043@gmail.com>
2025-07-05 00:50:56 -0300

    cambios

--- src/components/benefitsSection/BenefitsSection.jsx
+++ src/components/benefitsSection/BenefitsSection.jsx
@@ -5,13 +5,13 @@ const BenefitsSection = () => {
   return (
     <section id='beneficios' className='scroll-mt-[40px]  '>
       <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
-        <div className='relative isolate px-6 py-2 text-center sm:px-16 scroll-show'>
+        <div className='relative isolate px-6 py-2 text-center sm:px-16'>
           <div className='relative z-20'>
 
             <h2 className='max-w-3xl text-3xl tracking-tight text-left sm:text-center text-black mx-auto sm:text-3xl'>
               Mejorá tu presencia digital ahora
             </h2>
-            <div className='flex items-center justify-start sm:justify-center gap-x-6 mt-5'>
+            <div className='flex items-center justify-start sm:justify-center gap-x-6 mt-'>
               <Button icon={true} buttonStyle='px-4 py-3' label={'Cotizar'} />
             </div>
           </div>
